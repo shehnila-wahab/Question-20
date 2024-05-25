b1ead84d1d1c005dbba514e3f58989528d46bc83
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:06:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var languges = ["english", "urdu", "hindi", "french", "arabic"];
+console.log("list of languages:");
+for (var _i = 0, languges_1 = languges; _i < languges_1.length; _i++) {
+    var top_1 = languges_1[_i];
+    console.log(top_1);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let languges:string  [] = ["english","urdu","hindi","french","arabic"]
+console.log("list of languages:");
+for(let top of languges){
+    console.log(top);
+}
